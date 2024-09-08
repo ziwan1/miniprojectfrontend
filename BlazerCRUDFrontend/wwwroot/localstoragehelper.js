bf69728078a16d1aca09b133b7063677c7f0a42c
ziwan1 <rayhanalunantara2002@gmail.com>
2024-09-08 13:35:01 +0700

    Initial commit

---
+++ BlazerCRUDFrontend/wwwroot/localstoragehelper.js
@@ -0,0 +1,8 @@
+﻿window.localStorageHelper = {
+    setItem: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    getItem: function (key) {
+        return localStorage.getItem(key);
+    }
+};
